8656ccf8b859a1d01960022a434c481f653f04e3
Max Giga <gigamaximwachau@gmail.com>
2022-10-22 14:32:54 +0300

    Updates

---
+++ _app/immutable/chunks/1-de3d08c6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-8c4e6e1a.js";export{t as component};
--- _app/immutable/chunks/2-40207cba.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-e2b3c657.js";export{t as component};
