e1223b2b6424d645b62338dd621b982dfecbe7b5
Max Giga <gigamaximwachau@gmail.com>
2022-10-26 17:28:07 +0300

    initial setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware::Logger, web, App, HttpServer, Responder};
+use std::error::Error;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv::dotenv()?;
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .wrap(Logger::new("%a %{User-Agent}i"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await?;
+    Ok(())
 }
